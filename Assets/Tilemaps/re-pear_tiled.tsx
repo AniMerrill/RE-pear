d885b8dd08c28c2d20881e65de9263ec4cb9717f
trwiley <taylor.r.wiley@gmail.com>
2020-02-01 23:15:22 -0500

    Added .tsx files.

---
+++ Assets/Tilemaps/re-pear_tiled.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="re-pear_tiled.tsx" tilewidth="8" tileheight="8" tilecount="64" columns="8">
+ <image source="tiles.png" width="64" height="65"/>
+</tileset>
